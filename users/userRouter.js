43e289085a1261027f52ebb92f028f15a221bc6b
RobertPedersenDev <rob84044@gmail.com>
2019-07-30 23:46:32 -0600

    Restructured .get /:id within userRouter.js

--- users/userRouter.js
+++ users/userRouter.js
@@ -46,20 +46,22 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
+    user = req.user;
+    res.status(200).json(user);
+});
+
+router.get('/:id/posts', validateUserId, (req, res) => {
     const id = req.params.id;
-    Users.getById(id)
-        .then(user => {
-            if (user) {
-                res.status(200).json({ user });
-            } else {
-                res.status(404).json({ message: "Cannot find the user" });
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ error: err, message: 'The user information could not be retrieved.' });
-        });
-})
+
+    db.getUserPosts(id).then(posts => {
+        res.status(200).json(posts);
+    }).catch(err => {
+        res
+            .status(500)
+            .json({ error: "The posts information could not be retrieved." });
+    })
+});
 
 
 router.get('/:id/posts', async(req, res) => {
