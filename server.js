41942bfae30a364008cef4b1d51dcb8c8b8e4d6f
RobertPedersenDev <rob84044@gmail.com>
2019-07-30 20:07:54 -0600

    Set up the server.js file and edited the userRouter file

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const userRouter = require('./users/userRouter.js')
 const server = express();
+
+
+server.use(logger)
+server.use(express.json)
+server.use('/api/users/', userRouter)
+
 
 server.get('/', (req, res) => {
     res.send(`<h2>Let's write some middleware!</h2>`)
@@ -8,7 +15,8 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+    const now = Date.now()
+    console.log(req.method, req.originalUrl, now)
 };
 
 module.exports = server;
